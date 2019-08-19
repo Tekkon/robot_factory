ff0a3bfa229713bb58b7fdcb67e7ad7abd7aabbd
Nikita Popov <ningen.tos@gmail.com>
2019-08-19 22:13:04 +0500

    name_generator_spec refacroring

--- spec/name_generator_spec.rb
+++ spec/name_generator_spec.rb
@@ -2,9 +2,9 @@ require 'minitest/autorun'
 require "minitest/spec"
 require_relative '../name_generator'
 
-include NameGenerator
-
 describe NameGenerator do
+  include NameGenerator
+
   describe "#generate_name" do
     it "returns a name based on a specific pattern" do
       assert /[A-Z][A-Z]\d\d\d/ === generate_name
