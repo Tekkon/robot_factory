0f03809f45ea3cfa2547162af50bb46c2ee4df4b
Nikita Popov <ningen.tos@gmail.com>
2019-08-27 17:39:31 +0500

    Added shared example for "raises an exception if the name is not unique" tests

--- spec/robot_factory_spec.rb
+++ spec/robot_factory_spec.rb
@@ -2,8 +2,11 @@ require 'minitest/autorun'
 require "minitest/spec"
 require_relative '../app/robot_factory'
 require_relative '../app/not_unique_value_error'
+require_relative '../app/robot_factory_spec_helper'
 
 describe RobotFactory do
+  include RobotFactorySpecHelper
+
   let(:name_generator) { MiniTest::Mock.new }
   let(:robot_factory) { RobotFactory.new(name_generator) }
 
@@ -18,31 +21,30 @@ describe RobotFactory do
     end
 
     it 'puts a new name to the @robot_names array' do
-      robot_factory.robot_names = []
       robot = robot_factory.create_robot
       assert robot_factory.robot_names.include?(robot.name)
     end
 
     it 'raises an exception if the name is not unique' do
-      name_generator.expect :generate_name, 'BD890'
-      name_generator.expect :generate_name, 'BD890'
-      robot_factory.robot_names = ["BD890"]
-
-      assert_raises NotUniqueValueError do
+      not_unique_value_error_test do
         robot_factory.create_robot
       end
     end
   end
 
   describe '#reset_robot' do
-    before do
-      name_generator.expect :generate_name, 'XZ235'
-    end
-
     it "resets the robot's name" do
+      name_generator.expect :generate_name, 'XZ235'
       robot = robot_factory.create_robot
       robot_factory.reset_robot(robot)
       assert_equal robot.name, 'XZ235'
+    end
+
+    it 'raises an exception if the name is not unique' do
+      not_unique_value_error_test do
+        robot = robot_factory.create_robot
+        robot_factory.reset_robot(robot)
+      end
     end
   end
 end
